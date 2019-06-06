587d38b6957c6314794e2a3ca5039d198d86e740
Samuel Davis <sam.davis@alterimaging.com>
2019-06-06 15:53:20 -0400

    trigger note change handler on blur and keyup

--- index.php
+++ index.php
@@ -213,59 +213,73 @@ $paragraphs = array_reduce($files, function (array $paragraphs, string $filename
     <?php endif; ?>
 <?php endforeach; ?>
 <script type="application/javascript">
-  function injectNoteUI (from, to) {
-    const id = `${from}-${to}`
-    let input = document.querySelector(`#${id} > input`)
-    if (!input) {
-      const container = document.createElement('div')
-      container.setAttribute('id', id)
-      const label = document.createElement('small')
-      label.textContent = [...new Set([from, to].map((id) => id.slice(1).split('_').shift()))].join(' - ')
-      input = document.createElement('textarea')
-      input.addEventListener('blur', function (e) {
-        const value = e.target.value.trim()
-        if (!value) {
-          container.parentNode.removeChild(container)
-          localStorage.removeItem(id)
-        } else {
-          localStorage.setItem(id, value)
-        }
-      })
-
-      container.appendChild(label)
-      container.appendChild(input)
-
-      const toNode = document.getElementById(to)
-      let sibling = toNode.nextSibling
-      while (sibling && sibling.tagName !== 'SPAN')
-        sibling = sibling.nextSibling
-      toNode.parentNode.insertBefore(container, sibling)
+  document.addEventListener('DOMContentLoaded', function () {
+    function onNoteChange (e) {
+      const container = e.target.parentNode
+      const id = container.getAttribute('id')
+      const value = e.target.value.trim()
+      if (!value) {
+        container.parentNode.removeChild(container)
+        localStorage.removeItem(id)
+      } else {
+        localStorage.setItem(id, value)
+      }
     }
 
-    input.focus()
-  }
+    function injectNoteUI (from, to) {
+      const id = `${from}-${to}`
+      let input = document.querySelector(`#${id} > input`)
+      if (!input) {
+        const container = document.createElement('div')
+        container.setAttribute('id', id)
+        const label = document.createElement('small')
+        label.textContent = [...new Set([from, to].map((id) => id.slice(1).split('_').shift()))].join(' - ')
+        input = document.createElement('textarea')
+        input.value = localStorage.getItem(id)
+        input.addEventListener('blur', onNoteChange)
+        input.addEventListener('keyup', onNoteChange)
 
-  function quote () {
-    const selection = window.getSelection()
-    const range = selection.rangeCount ? selection.getRangeAt(0) : document.createRange()
+        container.appendChild(label)
+        container.appendChild(input)
 
-    let from = null
-    let to = null
-    const spans = document.getElementsByTagName('span')
-    for (let i = 0; i < spans.length; i++) {
-      if (range.intersectsNode(spans[i]))
-        to = spans[i]
-      if (from === null)
-        from = to
-      if (spans[i] === selection.focusNode)
-        break
+        const toNode = document.getElementById(to)
+        let sibling = toNode.nextSibling
+        while (sibling && sibling.tagName !== 'SPAN')
+          sibling = sibling.nextSibling
+        toNode.parentNode.insertBefore(container, sibling)
+      }
+
+      input.focus()
     }
 
-    if (from && to)
-      injectNoteUI(from.getAttribute('id'), to.getAttribute('id'))
-  }
+    function quote () {
+      const selection = window.getSelection()
+      const range = selection.rangeCount ? selection.getRangeAt(0) : document.createRange()
 
-  window.addEventListener('mouseup', quote)
+      let from = null
+      let to = null
+      const spans = document.getElementsByTagName('span')
+      for (let i = 0; i < spans.length; i++) {
+        if (range.intersectsNode(spans[i]))
+          to = spans[i]
+        if (from === null)
+          from = to
+        if (spans[i] === selection.focusNode)
+          break
+      }
+
+      if (from && to)
+        injectNoteUI(from.getAttribute('id'), to.getAttribute('id'))
+    }
+
+    window.addEventListener('mouseup', quote)
+
+    for (let id in localStorage) {
+      const [from, to] = id.split('-')
+      if (document.getElementById(from) && document.getElementById(to))
+        injectNoteUI(from, to)
+    }
+  })
 </script>
 </body>
 </html>
